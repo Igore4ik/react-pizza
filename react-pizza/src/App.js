cfc1bf5750d4292a707a97191a3fe848b15c54dc
Ihor Triapitsyn <tryapitsin1990@gmail.com>
2021-01-01 22:10:17 +0200

    getting info from server, create PizzaBlock

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -7,14 +7,21 @@ import Cart from "./pages/Cart";
 
 
 function App() {
-
+ const [pizzas, setPizzas] = React.useState([]);
+    React.useEffect(()=>{
+        fetch('http://localhost:3000/db.json')
+            .then(response=> response.json())
+            .then(json=>{
+                setPizzas(json.pizzas);
+            })
+    },[])
   return (
     <div className="App">
       <div className="wrapper">
          <Header/>
 
         <div className="content">
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" render={()=><Home items={pizzas}/>}/>
           <Route exact path="/cart" component={Cart}/>
 
         </div>
